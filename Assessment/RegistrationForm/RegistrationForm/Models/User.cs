1297bcf8ec33a96d5b1b18c056d264753726a2f1
willbelt <willbeltit@gmail.com>
2019-01-14 12:12:59 -0600

    Automation Challenge Complete

---
+++ Assessment/RegistrationForm/RegistrationForm/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistrationForm.Models
+{
+    public class User
+    {
+        [Required]
+        public string firstName { get; set; }
+        [Required]
+        public string lastName { get; set; }
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public string phone { get; set; }
+        [Required]
+        public string address { get; set; }
+        [Required]
+        public string company { get; set; }
+        [Required]
+        public string role { get; set; }
+    }
+}
